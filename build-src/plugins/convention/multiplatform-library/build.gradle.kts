bf6065acbb7fabcae532404d4bdd38f7e1851819
CXwudi <cx2010017@gmail.com>
2024-06-04 18:35:23 -0400

    multiplatform library setup is pretty much done

--- build-src/plugins/convention/multiplatform-library/build.gradle.kts
+++ build-src/plugins/convention/multiplatform-library/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   `kotlin-dsl`
+  alias(libs.plugins.buildConfig)
 }
 
 dependencies {
@@ -7,4 +8,11 @@ dependencies {
   implementation(libs.pluginDep.android)
   implementation(libs.pluginDep.compose)
   implementation(libs.pluginDep.kotlinCompose)
+}
+
+buildConfig {
+  generateAtSync = false
+  forClass(packageName = "my.util", className = "Versions") {
+    buildConfigField(Int::class.java, "JavaVersion", libs.versions.java.map { it.toInt() })
+  }
 }
--- build-src/plugins/convention/multiplatform-library/src/main/kotlin/my/multiplatform-library.gradle.kts
+++ build-src/plugins/convention/multiplatform-library/src/main/kotlin/my/multiplatform-library.gradle.kts
@@ -1,8 +1,54 @@
 package my
 
+import my.util.Versions
+
 plugins {
   kotlin("multiplatform")
   id("com.android.library")
   id("org.jetbrains.compose")
   kotlin("plugin.compose")
+}
+
+kotlin {
+  androidTarget()
+  jvm()
+  js(IR) {
+    browser()
+  }
+
+  listOf(
+    iosX64(),
+    iosArm64(),
+    iosSimulatorArm64()
+  ).forEach {
+    it.binaries.framework {
+      baseName = "shared"
+      isStatic = true
+    }
+  }
+
+  applyDefaultHierarchyTemplate()
+
+  sourceSets {
+    val commonJvmMain by creating {
+      dependsOn(commonMain.get())
+    }
+
+    jvmMain.get().dependsOn(commonJvmMain)
+    androidMain.get().dependsOn(commonJvmMain)
+  }
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(Versions.JavaVersion)
+  }
+}
+
+android {
+  compileSdk = 34
+  defaultConfig {
+    minSdk = 26
+  }
+  // jvm version is covered by java toolchain above
 }
