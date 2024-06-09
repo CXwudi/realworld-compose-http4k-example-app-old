c034707bd24ade6baf60bf80fa1ff8a5182427ce
CXwudi <cx2010017@gmail.com>
2024-06-09 09:51:31 -0400

    comment

--- conduit-frontend/build.gradle.kts
+++ conduit-frontend/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+  // do all the declaration here to avoid warning
   alias(libs.plugins.kotlinMultiplatform) apply false
   alias(libs.plugins.kotlinJvm) apply false
   alias(libs.plugins.androidLibrary) apply false
