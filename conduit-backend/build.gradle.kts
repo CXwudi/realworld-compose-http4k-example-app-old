748b31bf161aae42e56fddd87f656b240bb933b3
CXwudi <cx2010017@gmail.com>
2024-06-18 21:51:45 -0400

    :sparkles: Add new dependencies

--- conduit-backend/build.gradle.kts
+++ conduit-backend/build.gradle.kts
@@ -24,10 +24,21 @@ dependencies {
   implementation(libs.dev.serializationJson)
   implementation(libs.dev.koin)
 
+  // https://toolbox.http4k.org/stack/dD1BQU1BWlFETEFTOEItQU1sQTRZRDZBUHBCTEFGRmcmYz1NYWluJnA9bWlrdWZhbi5jeC5jb25kdWl0LmJhY2tlbmQ
   implementation(platform(libs.dev.http4kBom))
   implementation("org.http4k:http4k-core")
   implementation("org.http4k:http4k-format-kotlinx-serialization")
   implementation("org.http4k:http4k-metrics-micrometer")
+  implementation(platform(libs.dev.exposedBom))
+  implementation("org.jetbrains.exposed:exposed-core")
+  implementation("org.jetbrains.exposed:exposed-dao")
+  implementation("org.jetbrains.exposed:exposed-jdbc")
+  implementation(libs.dev.h2)
+  implementation(libs.dev.hikari)
+  implementation(libs.dev.flyway)
+  implementation(libs.dev.inlineLogging)
+  implementation(libs.dev.slf4j)
+  runtimeOnly(libs.dev.logback)
 
   testImplementation("org.http4k:http4k-testing-approval")
   testImplementation("org.http4k:http4k-testing-hamkrest")
