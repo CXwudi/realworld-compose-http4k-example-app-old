337069da2ba75c8c2a047c8feb253cf4eec7a1e4
CXwudi <cx2010017@gmail.com>
2024-06-21 17:49:33 -0400

    ✨ Add server initialization and complete the server setup.
    Include `ConduitServer` in Bootstrap for server startup. Update Main.kt to organize Koin modules by layers.

--- conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/Bootstrap.kt
+++ conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/Bootstrap.kt
@@ -1,13 +1,15 @@
 package mikufan.cx.conduit.backend
 
+import mikufan.cx.conduit.backend.controller.ConduitServer
 import org.flywaydb.core.Flyway
 
 class Bootstrap(
   private val flyway: Flyway,
+  private val server: ConduitServer
 ) : Runnable {
   override fun run() {
     dbMigration()
-
+    server.start()
   }
 
   private fun dbMigration() {
--- conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/Main.kt
+++ conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/Main.kt
@@ -1,8 +1,9 @@
 package mikufan.cx.conduit.backend
 
 import mikufan.cx.conduit.backend.config.configModule
+import mikufan.cx.conduit.backend.controller.allHttpSetupModule
 import mikufan.cx.conduit.backend.db.dbModule
-import mikufan.cx.inlinelogging.KInlineLogging
+import mikufan.cx.conduit.backend.service.serviceModule
 import org.koin.core.KoinApplication
 import org.koin.core.logger.Level
 import org.koin.core.module.dsl.singleOf
@@ -10,12 +11,17 @@ import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import org.koin.logger.slf4jLogger
 
-
 val bootstrapModule = module {
   singleOf(::Bootstrap)
 }
 
-val allModules = listOf(configModule, dbModule, bootstrapModule)
+val allModules = listOf(
+  configModule, // configuration layer, can be used by anyone in any later
+  dbModule, // repo layer, but mostly just db related
+  serviceModule, // service layer, handling business logic
+  allHttpSetupModule, // controller layer
+  bootstrapModule // bootstrap layer
+)
 
 fun initKoin(): KoinApplication {
   val koin = koinApplication {
@@ -30,5 +36,3 @@ fun main(args: Array<String>) {
   koinApp.koin.get<Bootstrap>().run()
   koinApp.close()
 }
-
-private val log = KInlineLogging.logger()
