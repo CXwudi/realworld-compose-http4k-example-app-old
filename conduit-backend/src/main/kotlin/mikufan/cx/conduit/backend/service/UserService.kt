b1fa07cc85539b53621b50512494c3e803093acc
CXwudi <cx2010017@gmail.com>
2024-06-21 17:49:40 -0400

    🎉 Add logging for successful user creation in UserService
    Include KInlineLogging to log the creation of new users, enhancing traceability and debugging.

--- conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/service/UserService.kt
+++ conduit-backend/src/main/kotlin/mikufan/cx/conduit/backend/service/UserService.kt
@@ -6,6 +6,7 @@ import mikufan.cx.conduit.backend.db.tx
 import mikufan.cx.conduit.backend.util.ConduitException
 import mikufan.cx.conduit.common.NewUserDto
 import mikufan.cx.conduit.common.UserDto
+import mikufan.cx.inlinelogging.KInlineLogging
 
 class UserService(
   private val txManager: TransactionManager,
@@ -18,7 +19,11 @@ class UserService(
       if (userDto != null) {
         throw ConduitException("User already exists, username or email must be unique")
       } else {
-        userRepo.insert(user) ?: throw ConduitException("Failed to create new user")
+        val newUser = userRepo.insert(user) ?: throw ConduitException("Failed to create new user ${user.username}")
+        log.info { "Successfully created new user ${user.username}" }
+        newUser
       }
     }
 }
+
+private val log = KInlineLogging.logger()
