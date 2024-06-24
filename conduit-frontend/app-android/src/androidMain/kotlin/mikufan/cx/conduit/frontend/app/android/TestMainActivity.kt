31509263fa3a1e297a38cd6e2c78adbf2dcf9d27
CXwudi <cx2010017@gmail.com>
2024-06-23 23:01:12 -0400

    🚀 Bootstrap RootComponent in all platforms
    This commit integrates the RootComponent into Android, Desktop, and JS platforms, setting up the foundation for a unified UI structure across all platforms.

--- conduit-frontend/app-android/src/androidMain/kotlin/mikufan/cx/conduit/frontend/app/android/TestMainActivity.kt
+++ conduit-frontend/app-android/src/androidMain/kotlin/mikufan/cx/conduit/frontend/app/android/TestMainActivity.kt
@@ -3,13 +3,22 @@ package mikufan.cx.conduit.frontend.app.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.defaultComponentContext
+import mikufan.cx.conduit.frontend.logic.DefaultRootComponent
+import mikufan.cx.conduit.frontend.ui.RootScreen
 import mikufan.cx.conduit.frontend.ui.TestMainUI
 
 class TestMainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    val defaultComponentContext = defaultComponentContext()
+    val rootComponent = DefaultRootComponent(defaultComponentContext)
     setContent {
-      TestMainUI()
+      TestMainUI {
+        RootScreen(rootComponent)
+      }
     }
   }
 }
--- conduit-frontend/app-js/src/jsMain/kotlin/mikufan/cx/conduit/frontend/app/js/TestMain.kt
+++ conduit-frontend/app-js/src/jsMain/kotlin/mikufan/cx/conduit/frontend/app/js/TestMain.kt
@@ -2,14 +2,27 @@ package mikufan.cx.conduit.frontend.app.js
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import mikufan.cx.conduit.frontend.logic.DefaultRootComponent
+import mikufan.cx.conduit.frontend.ui.RootScreen
 import mikufan.cx.conduit.frontend.ui.TestMainUI
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main(args: Array<String>) {
+
+  val lifecycle = LifecycleRegistry()
+  val defaultComponentContext = DefaultComponentContext(
+    lifecycle = lifecycle
+  )
+  val rootComponent = DefaultRootComponent(defaultComponentContext)
+
   onWasmReady {
     CanvasBasedWindow("Conduit") {
-      TestMainUI()
+      TestMainUI {
+        RootScreen(rootComponent)
+      }
     }
   }
 }
