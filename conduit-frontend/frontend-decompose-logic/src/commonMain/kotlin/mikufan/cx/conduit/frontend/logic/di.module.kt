3a4cc3207a12ee305e0e2a6506d5c5f07cdccaee
CXwudi <cx2010017@gmail.com>
2024-06-25 23:41:40 -0400

    🎉 Add essentyModule with Lifecycle, StateKeeper, and InstanceKeeper
    Integrate essential lifecycle and state management modules into DI setup.

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/di.module.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/di.module.kt
@@ -1,2 +1,12 @@
 package mikufan.cx.conduit.frontend.logic
 
+import com.arkivanov.essenty.instancekeeper.InstanceKeeperDispatcher
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.statekeeper.StateKeeperDispatcher
+import org.koin.dsl.module
+
+val essentyModule = module {
+  single { LifecycleRegistry() }
+  single { StateKeeperDispatcher() }
+  single { InstanceKeeperDispatcher() }
+}
