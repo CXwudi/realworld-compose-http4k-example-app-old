9249ae38aadf084a422c6606dab7031c15b408e2
CXwudi <cx2010017@gmail.com>
2024-06-25 23:07:23 -0400

    ✨ Created MviComponent interface for better encapsulation and functionality
    Add generics to MviComponent interface to accept Intent and State, and implement ComponentContext.

---
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/util/MviComponent.kt
@@ -0,0 +1,21 @@
+package mikufan.cx.conduit.frontend.logic.util
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.value.Value
+
+/**
+ * A single MVI component that accept an [Intent] and update the exposed single [State] value.
+ *
+ * Suitable for [com.arkivanov.decompose.Component] that is used for a single UI.
+ *
+ * Probably not suitable for [com.arkivanov.decompose.Component] that is used for navigation.
+ * Although you can and it will break the MVI pattern as you will have another exposed value for navigation.
+ *
+ * @param Intent
+ * @param State
+ * @constructor Create empty Mvi component
+ */
+interface MviComponent<Intent, State : Any> : ComponentContext {
+  val state: Value<State>
+  fun send(intent: Intent)
+}
