ad1210af9c71a2d964f4cfa92cb4595073518f1a
CXwudi <cx2010017@gmail.com>
2024-06-28 18:30:15 -0400

    :white_check_mark: Add mockk library for testing
    This commit adds the mockk library as a test dependency to enhance our testing capabilities with powerful mocking features.

--- conduit-backend/build.gradle.kts
+++ conduit-backend/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
   testImplementation(platform(libs.dev.junitBom))
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testImplementation("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation(libs.dev.mockk)
 }
 
 application {
