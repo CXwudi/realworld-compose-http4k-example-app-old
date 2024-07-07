77cd064a77465a54f26219bfab646211ffb97666
CXwudi <cx2010017@gmail.com>
2024-07-07 17:04:46 -0400

    ✨ Implement animated transitions and child slot handling in RootNavigation
    Enhance the RootNavigation with animated transitions and implement child slot handling for dynamic content rendering.

--- conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/RootNavigation.kt
+++ conduit-frontend/frontend-compose-ui/src/commonMain/kotlin/mikufan/cx/conduit/frontend/ui/screen/RootNavigation.kt
@@ -1,10 +1,53 @@
 package mikufan.cx.conduit.frontend.ui.screen
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import mikufan.cx.conduit.frontend.logic.component.DefaultRootNavComponent
+import mikufan.cx.conduit.frontend.logic.component.RootComponentChild
 
 @Composable
 fun RootNavigation(rootComponent: DefaultRootNavComponent, modifier: Modifier = Modifier) {
+  val childSlot by rootComponent.childSlot.subscribeAsState()
 
+  AnimatedContentTransition(
+    targetState = childSlot.child?.instance,
+    modifier = modifier
+  ) {
+    when (it) {
+      is RootComponentChild.Loading -> LoadingScreen()
+      is RootComponentChild.LandingPage -> LandingPage(it.component)
+      else -> Unit
+    }
+  }
+}
+
+@Composable
+private fun <S> AnimatedContentTransition(
+  targetState: S,
+  modifier: Modifier = Modifier,
+  content: @Composable AnimatedContentScope.(targetState: S) -> Unit
+) {
+  AnimatedContent(
+    targetState = targetState,
+    modifier = modifier,
+    transitionSpec = {
+      slideIntoContainer(
+        towards = SlideDirection.Left,
+        animationSpec = tween(),
+      ).togetherWith(
+        slideOutOfContainer(
+          towards = SlideDirection.Right,
+          animationSpec = tween()
+        )
+      )
+    },
+    content = content
+  )
 }
--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/RootNavComponent.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/RootNavComponent.kt
@@ -1,10 +1,27 @@
 package mikufan.cx.conduit.frontend.logic.component
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.slot.ChildSlot
+import com.arkivanov.decompose.router.slot.SlotNavigation
+import com.arkivanov.decompose.router.slot.childSlot
+import com.arkivanov.decompose.value.Value
+import kotlinx.serialization.Serializable
+import mikufan.cx.conduit.frontend.logic.component.landing.DefaultLandingPageComponent
+import mikufan.cx.conduit.frontend.logic.component.landing.LandingPageComponent
 import mikufan.cx.conduit.frontend.logic.component.util.LocalKoinComponent
+import org.koin.core.component.get
 
 interface RootNavComponent {
-//  val child: Value<ChildStack<*, RootComponentChild>>
+  val childSlot: Value<ChildSlot<*, RootComponentChild>>
+}
+
+sealed interface RootComponentChild {
+
+  data object Loading : RootComponentChild
+  data class LandingPage(
+    val component: LandingPageComponent
+  ) : RootComponentChild
+
 }
 
 class DefaultRootNavComponent(
@@ -12,4 +29,44 @@ class DefaultRootNavComponent(
   private val koin: LocalKoinComponent
 ) : RootNavComponent, ComponentContext by componentContext {
 
+  private val slotNavigation = SlotNavigation<Config>()
+
+  override val childSlot: Value<ChildSlot<*, RootComponentChild>> =
+    childSlot(
+      source = slotNavigation,
+      initialConfiguration = { Config.Loading },
+      serializer = Config.serializer(),
+      childFactory = ::childFactory
+    )
+
+  private fun childFactory(
+    config: Config,
+    componentContext: ComponentContext
+  ): RootComponentChild {
+    val child = when (config) {
+      Config.Loading -> RootComponentChild.Loading
+      Config.LandingPage -> RootComponentChild.LandingPage(
+        component = koin.createLandingPageComponent(componentContext)
+      )
+    }
+    return child
+  }
+
+  private fun LocalKoinComponent.createLandingPageComponent(
+    componentContext: ComponentContext
+  ) = DefaultLandingPageComponent(
+    componentContext = componentContext,
+    koinComponent = this,
+    storeFactory = get()
+  )
+
+
+  @Serializable
+  sealed interface Config {
+    @Serializable
+    data object Loading : Config
+
+    @Serializable
+    data object LandingPage : Config
+  }
 }
