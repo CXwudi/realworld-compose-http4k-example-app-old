7a3409c685d9ff2fae2616e380ccbae34b55c938
CXwudi <cx2010017@gmail.com>
2024-07-07 17:00:57 -0400

    🔍 Document KoinComponent usage
    Add documentation for LocalKoinComponent to explain its purpose and usage within Decompose components.

--- conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/KoinUtil.kt
+++ conduit-frontend/frontend-decompose-logic/src/commonMain/kotlin/mikufan/cx/conduit/frontend/logic/component/util/KoinUtil.kt
@@ -4,6 +4,9 @@ import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
 
 /**
+ * A [KoinComponent] that stores a reference to the [Koin] instance in the [LocalKoinComponent].
+ * Mainly used in Decompose component to get access to the [Koin] instance in a more test-friendly way.
+ *
  * Idea copied from https://gist.github.com/aartikov/30e182fd58ed9697af498bb22ef4edfa
  */
 class LocalKoinComponent(
